f1ea317c7001e5ab29f7a3cd42ee13bc2e87911e
imotako-pumpkin <imotako_pumpkin@outlook.com>
2023-11-09 23:16:25 +0900

    base

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,76 @@
+import { HamburgerIcon, MoonIcon, SunIcon } from '@chakra-ui/icons'
+import { Link } from '@chakra-ui/next-js'
+import {
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  IconButton,
+  useDisclosure,
+  Stack,
+  useColorMode,
+  Spacer,
+  Flex,
+  DrawerOverlay,
+} from '@chakra-ui/react'
+import { FC } from 'react'
+
+export const Header: FC = () => {
+  return (
+    <>
+      <Flex h={16} position='fixed' top={0} left={0} right={0} zIndex={999}>
+        <Spacer />
+        <Menu />
+      </Flex>
+      <Spacer h={16} />
+    </>
+  )
+}
+
+const Menu: FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { colorMode, toggleColorMode } = useColorMode()
+  return (
+    <>
+      {!isOpen && (
+        <>
+          <IconButton
+            aria-label='color-mode'
+            icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+            size={['md', 'md', 'lg', 'lg', 'lg']}
+            m={4}
+            onClick={toggleColorMode}
+            backgroundColor={'transparent'}
+          />
+          <IconButton
+            aria-label='hamburger'
+            icon={<HamburgerIcon />}
+            onClick={() => onOpen()}
+            size={['md', 'md', 'lg', 'lg', 'lg']}
+            m={4}
+            backgroundColor={'transparent'}
+          />
+        </>
+      )}
+
+      <Drawer onClose={onClose} isOpen={isOpen} size='md'>
+        <DrawerOverlay />
+        <DrawerContent bgColor={colorMode === 'light' ? 'whiteAlpha.900' : 'blackAlpha.900'}>
+          <DrawerCloseButton />
+          <DrawerHeader>Menu</DrawerHeader>
+          <DrawerBody>
+            <Stack>
+              <Link href='/' onClick={() => onClose()} fontSize='5xl'>
+                HOME
+              </Link>
+              <Link href={'/line'} onClick={() => onClose()} fontSize='5xl'>
+                LINE
+              </Link>
+            </Stack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
+import { Header } from '@/components/layouts/Header'
 import '@/styles/globals.css'
+import { Container, ChakraProvider, extendTheme, Spacer } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
+const breakpoints = { sm: '30em', md: '48em', lg: '62em', xl: '80em', '2xl': '96em' }
+
+const theme = extendTheme({
+  breakpoints,
+  styles: {
+    global: (props: { colorMode: string }) => ({
+      body: {
+        bgGradient:
+          props.colorMode === 'light'
+            ? 'linear(to-r, gray.300, yellow.400, pink.200)'
+            : 'linear(to-l, #7928CA, #FF0080)',
+      },
+    }),
+  },
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={extendTheme(theme)}>
+      <Header />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
